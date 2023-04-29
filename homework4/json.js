b9a8f79d2061d248c4601a2f367f30fdadfcb972
Yakov Krasner <yakov.krasner@yandex.ru>
2023-04-29 23:00:04 +0700

    JSON templating  added

---
+++ homework4/json.js
@@ -0,0 +1,39 @@
+const data = `[
+    {
+        "imgSrc": "img/product1.jpg",
+        "nameOfProduct": "ELLERY XMO CAPSULE",
+        "descriptionOfProduct": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "priceOfProduct": "$52.00"
+    },
+    {
+        "imgSrc": "img/product2.png",
+        "nameOfProduct": "ELLERY XMO CAPSULE",
+        "descriptionOfProduct": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "priceOfProduct": "$52.00"
+    },
+    {
+        "imgSrc": "img/product3.png",
+        "nameOfProduct": "ELLERY XMO CAPSULE",
+        "descriptionOfProduct": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "priceOfProduct": "$52.00"
+    },
+
+    {
+        "imgSrc": "img/product4.png",
+        "nameOfProduct": "ELLERY XMO CAPSULE",
+        "descriptionOfProduct": "Я ОБОЖАЮ JSON!!!!.",
+        "priceOfProduct": "$52.00"
+    },
+    {
+        "imgSrc": "img/product5.png",
+        "nameOfProduct": "ELLERY XMO CAPSULE",
+        "descriptionOfProduct": "Я ОБОЖАЮ JSON!!!!.",
+        "priceOfProduct": "$52.00"
+    },
+    {
+        "imgSrc": "img/product6.png",
+        "nameOfProduct": "ELLERY XMO CAPSULE",
+        "descriptionOfProduct": "Я ОБОЖАЮ JSON!!!!.",
+        "priceOfProduct": "$52.00"
+    }
+]`   
