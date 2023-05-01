da44c461b6f848ae1455ea31428abeb7c152bef6
Yakov Krasner <yakov.krasner@yandex.ru>
2023-05-01 14:54:11 +0700

    Cart added

--- homework4/json.js
+++ homework4/json.js
@@ -1,38 +1,56 @@
 const data = `[
     {
+        "id": "ID1",
         "imgSrc": "img/product1.jpg",
         "nameOfProduct": "ELLERY XMO CAPSULE",
+        "color": "black",
+        "size": "XL",
         "descriptionOfProduct": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
         "priceOfProduct": "$52.00"
     },
-    {
+    {   
+        "id": "ID2",
         "imgSrc": "img/product2.png",
         "nameOfProduct": "ELLERY XMO CAPSULE",
+        "color": "white",
+        "size": "XL",
         "descriptionOfProduct": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "priceOfProduct": "$52.00"
+        "priceOfProduct": "52.00"
     },
-    {
+    {   
+        "id": "ID3",
         "imgSrc": "img/product3.png",
         "nameOfProduct": "ELLERY XMO CAPSULE",
+        "color": "yellow",
+        "size": "XL",
         "descriptionOfProduct": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
         "priceOfProduct": "$52.00"
     },
 
-    {
+    {   
+        "id": "ID4",
         "imgSrc": "img/product4.png",
         "nameOfProduct": "ELLERY XMO CAPSULE",
+        "color": "red",
+        "size": "XL",
         "descriptionOfProduct": "Я ОБОЖАЮ JSON!!!!.",
         "priceOfProduct": "$52.00"
     },
-    {
+    {   
+        "id": "ID5",
         "imgSrc": "img/product5.png",
         "nameOfProduct": "ELLERY XMO CAPSULE",
+        "color": "blue",
+        "size": "XL",
         "descriptionOfProduct": "Я ОБОЖАЮ JSON!!!!.",
         "priceOfProduct": "$52.00"
     },
-    {
+    {   
+        "id": "ID6",
         "imgSrc": "img/product6.png",
         "nameOfProduct": "ELLERY XMO CAPSULE",
+        "color": "any",
+        "size": "XL",
         "descriptionOfProduct": "Я ОБОЖАЮ JSON!!!!.",
         "priceOfProduct": "$52.00"
     }
